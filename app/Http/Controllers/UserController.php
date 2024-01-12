1530cfac336701745ea9bb8caff8d59209782cf1
FCL00 <flagahit00@gmail.com>
2024-01-12 09:05:09 +0800

    add UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use inertia;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+
+    function showRegister(){
+        return inertia("Register");
+    }
+
+    function Register(){
+        return inertia("Login");
+    }
+    
+    function Login(){
+        return inertia("Login");
+    }
+
+
+    function showLogin(){
+        return inertia("Login");
+    }
 }
